91811d8aec1030d1a0ac56e4b3159de57da12b2f
karissa <krmckelv@gmail.com>
2017-06-19 15:11:26 -0700

    add archiver to the dat-registry-api

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const Api = require('./api')
 const Auth = require('./auth')
 const database = require('./database')
+const Archiver = require('./archiver')
 
 module.exports = function (config) {
   var db = config.database || database(config.db)
   const auth = Auth(config, db)
   const api = Api(auth, db)
+  const archiver = config.dats || Archiver(config.archiver)
 
   return {
     db: db,
+    archiver: archiver,
     users: api.users,
     dats: api.dats,
     auth: auth,
